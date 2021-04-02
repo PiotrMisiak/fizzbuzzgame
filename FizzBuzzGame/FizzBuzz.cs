942ee5a6951f64c036ba0e6ffc1c2f7434b35f64
PiotrMisiak <piotrektrumpet@gmail.com>
2021-04-03 00:31:55 +0200

    Add project files.

---
+++ FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class FizzBuzz
+    {
+        public string GameRules(int number)
+        {          
+              
+                if (number % 3 == 0 && number % 5 == 0)
+               
+                    return "FizzBuzz";
+                
+                if (number % 5 == 0)
+                
+                    return "Buzz";
+
+                if (number % 3 == 0)
+
+                return "Fizz";
+
+                else
+                
+                return number.ToString();
+                
+            
+        }
+        
+       
+    }
+}
---
+++ FizzBuzzGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {                      
+            
+            var fizzbuzz = new FizzBuzz();
+            
+            Console.WriteLine("Podaj liczbę i zagraj w grę FizzBuzz.");
+            
+            while (true)
+            {               
+                var number = GetNumber();
+                Console.WriteLine(fizzbuzz.GameRules(number));
+                Console.WriteLine("\nPodaj następną liczbę.\nJeżeli chcesz zakończyć grę wpisz: t");
+            }
+            
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                var answer = Console.ReadLine();
+                if (answer.ToUpper() == "T")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(answer, out int number))
+                {
+                    Console.WriteLine("\nPodałeś nieprawidłowe dane, spróbuj jeszcze raz.");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+
+}
